93667f1d0c0a6ab8a0e1d10a656ca2fb38835b23
Nirajan Thapa <thapanirajan789@gmail.com>
2025-11-12 17:29:00 +0545

    order cancel

--- src/Pages/EsewaPaymentFailure.tsx
+++ src/Pages/EsewaPaymentFailure.tsx
@@ -26,7 +26,7 @@ const EsewaPaymentFailure: React.FC = () => {
             }
 
             try {
-                const response = await fetch(`${API_BASE_URL}/api/order/payment/cancel`, {
+                const response = await fetch(`${API_BASE_URL}/api/order/esewa/fail`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
